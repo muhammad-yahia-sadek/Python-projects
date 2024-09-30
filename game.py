2fc2e7e0eef48d49d11e031635f29cc5a3c1808f
Muhammad Sadek <mohamed.sadek@intcore.com>
2024-09-30 12:42:16 +0300

    new/python

---
+++ game.py
@@ -0,0 +1,37 @@
+print ("""
+
+        ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣴⣿⣿⡿⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣾⣿⣿⠟⠁⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⢀⣠⣤⣤⣤⣀⣀⠈⠋⠉⣁⣠⣤⣤⣤⣀⡀⠀⠀
+⠀⢠⣶⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣦⡀
+⣠⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠟⠋⠀
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡏⠀⠀⠀
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇⠀⠀⠀
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣧⠀⠀⠀
+⠹⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣤⣀
+⠀⠻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠁
+⠀⠀⠙⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡟⠁⠀
+⠀⠀⠀⠈⠙⢿⣿⣿⣿⠿⠟⠛⠻⠿⣿⣿⣿⡿⠋⠀⠀⠀
+
+    Welcome to the apple Store for new iphone
+""")
+mobile = input("Which mobile you choose Pro 📱 or Normal and plus 📱\n").lower()
+if mobile == "pro":
+    print ("You choose Pro Catagory")
+    pro = input("Which colur you want to have ? Black 📱 , White 📱 , Gold 📱 \n").lower()
+    if pro == ("white"):
+       print ("The mobile not exiting can you ask letter")
+    elif pro == ("black"):
+        print("The mobile exting but it has an over price")
+    elif pro == ("gold"):
+        print ("Must order it to give for you")
+    else :
+        print("Invaild choose could you try again")
+else :
+    print ("would you get Normal or plus 📱")
+    normal_plus = input("Which colour you want of the mobile\n").lower()
+    if normal_plus == "pink":
+       print ("it is existing")
+    else:
+        print ("Ask support to help you")
